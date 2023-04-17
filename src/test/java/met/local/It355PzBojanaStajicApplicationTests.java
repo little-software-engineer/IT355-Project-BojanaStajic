12e8c7f11079d65e93dbdcf977b8f5dea6a345c1
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2023-04-17 14:31:28 +0200

    Update It355PzBojanaStajicApplicationTests.java

--- src/test/java/met/local/It355PzBojanaStajicApplicationTests.java
+++ src/test/java/met/local/It355PzBojanaStajicApplicationTests.java
@@ -1,1 +1,14 @@
+package met.local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355PzBojanaStajicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
 
